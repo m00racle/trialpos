2a379fed65765e11598972984dba635e21dae872
mooracle <yanuar.prakosa@gmail.com>
2020-07-08 13:21:32 +0700

    initial commit

---
+++ controller/controltemp.php
@@ -0,0 +1,19 @@
+<?php
+  /**
+   * class for template controller
+   */
+  class TempController
+  {
+
+    // function __construct(argument)
+    // {
+    //   // code...
+    // }
+    public function ctrTemplate()
+    {
+      // code...this is control for templates
+      include 'view\template.php';
+    }
+  }
+
+ ?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once "controller\controltemp.php";
+//instatiate a template object type templatecontroller;
+$temmplate = new TempController();
+//access the method to control the template
+$temmplate -> ctrTemplate();
+?>
