29781d0c8fdae796f720b1b30955a2ce1515c3f9
mooracle <yanuar.prakosa@gmail.com>
2020-07-12 11:53:05 +0700

    c1: build (static) user module pt 1

--- view/module/dashboard.php
+++ view/module/dashboard.php
@@ -6,7 +6,7 @@
       <small>Control Panel</small>
     </h1>
     <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li><a href="dashboard"><i class="fa fa-dashboard"></i> Home</a></li>
 
       <li class="active">Dashboard</li>
     </ol>
--- view/module/user.php
+++ view/module/user.php
@@ -6,7 +6,7 @@
       <small>Control Panel</small>
     </h1>
     <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li><a href="dashboard"><i class="fa fa-dashboard"></i> Home</a></li>
 
       <li class="active">User Management</li>
     </ol>
@@ -18,27 +18,92 @@
     <!-- Default box -->
     <div class="box">
       <div class="box-header with-border">
-        <h3 class="box-title">Title</h3>
+        <button class="btn btn-primary"
+         data-toggle="modal" data-target="#modalAddUser">Add User</button>
+         <!-- /.ini bootstrap modal untuk add user -->
 
-        <div class="box-tools pull-right">
-          <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                  title="Collapse">
-            <i class="fa fa-minus"></i></button>
-          <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-            <i class="fa fa-times"></i></button>
-        </div>
       </div>
       <div class="box-body">
-        Start creating your amazing application!
+        <!-- here we will write the table of all users; -->
+        <table class="table table-bordered table-striped">
+          <!-- the table classes are from the plugin -->
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Full Name</th>
+              <th>username</th>
+              <th>Picture</th>
+              <th>Role</th>
+              <th>Status</th>
+              <th>Last login</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>User Admin</td>
+              <td>admin</td>
+              <td><img src="view\img\user\default\anon_icon.png"
+                    class="img-thumbnail" width="40px"></td>
+              <td>admin</td>
+              <td><button class="btn btn-success btn-xs">Activate</button></td>
+              <td>2020-07-11 23:05:22</td>
+              <td>
+                <div class="btn-group">
+                  <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                  <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>User Admin</td>
+              <td>admin</td>
+              <td><img src="view\img\user\default\anon_icon.png"
+                    class="img-thumbnail" width="40px"></td>
+              <td>admin</td>
+              <td><button class="btn btn-danger btn-xs">Deactivate</button></td>
+              <td>2020-07-11 23:05:22</td>
+              <td>
+                <div class="btn-group">
+                  <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                  <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
       <!-- /.box-body -->
-      <div class="box-footer">
+      <!-- <div class="box-footer">
         Footer
-      </div>
+      </div> -->
       <!-- /.box-footer-->
     </div>
     <!-- /.box -->
 
   </section>
   <!-- /.content -->
+</div>
+<!-- this is the modal pop ups; -->
+<!-- Modal -->
+<div id="modalAddUser" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
 </div>
