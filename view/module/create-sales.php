a329981d60f3fe2b74601a41e62eb7e88a7b1923
mooracle <yanuar.prakosa@gmail.com>
2020-08-05 11:24:29 +0700

    c7-01: build sales module half finished

--- view/module/create-sales.php
+++ view/module/create-sales.php
@@ -14,31 +14,174 @@
 
   <!-- Main content -->
   <section class="content">
+    <div class="row">
+      <div class="col-lg-5 col-xs-12">
+        <div class="box box-success">
+          <!-- this is the initial default box colored as green meaning success but when things went bad it will turns yellow as warning -->
+          <div class="box-header with-border">
+            <!-- empty space just to fill some sort of white space -->
+          </div>
+          <div class="box-header with-border">
+            <form class="" action="" method="post" role="form">
+              <div class="box">
+                <!-- SELLER INPUT -->
+                <div class="form-group">
+                  <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    <input type="text" class="form-control" id="newSeller" name="newSeller" value="User Admin" readonly>
+                  </div>
+                </div>
+                <!-- BILLING CODE -->
+                <div class="form-group">
+                  <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                    <input type="text" class="form-control" id="newSale" name="newSale" value="100002343" readonly>
+                  </div>
+                </div>
+                <!-- CUSTOMER INPUT -->
+                <div class="form-group">
+                  <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
 
-    <!-- Default box -->
-    <div class="box">
-      <div class="box-header with-border">
-        <h3 class="box-title">Title</h3>
-
-        <div class="box-tools pull-right">
-          <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                  title="Collapse">
-            <i class="fa fa-minus"></i></button>
-          <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-            <i class="fa fa-times"></i></button>
+                    <select class="form-control" id="selectCustomer" name="selectCustomer" required>
+                      <option value="">Select Customer</option>
+                    </select>
+                    <!-- add button for new customer -->
+                    <span class="input-group-addon"><button type="button" class="btn btn-default btn-xs" data-toggle="modal" data-target="#modalAddCustomer" data-dismiss="modal">Add Customer</button></span>
+                  </div>
+                </div>
+                <!-- ENTRY FOR NEW PRODUCT -->
+                <div class="form-group row newProduct">
+                  <!-- description of the product -->
+                  <div class="col-xs-6" style="padding-right:0px">
+                    <div class="input-group">
+                      <span class="input-group-addon"><button class="btn btn-danger"><i class="fa fa-times"></i></button></span>
+                    <input type="text" class="form-control" id="addProduct" name="addProduct" placeholder="Product Description" required>
+                    </div>
+                  </div>
+                  <div class="col-xs-3">
+                    <!-- product quantity -->
+                    <input type="number" class="form-control" id="newProductQuantity" name="newProductQuantity" min="1" placeholder="0" required>
+                  </div>
+                  <div class="col-xs-3" style="padding-left:0px">
+                    <!-- product price -->
+                    <div class="input-group">
+                      <input type="number" class="form-control" id="newProductPrice" name="newProductPrice" min="1" placeholder="00000" required readonly>
+                      <span class="input-group-addon"><i class="ion ion-social-usd"></i></span>
+                    </div>
+                  </div>
+                </div>
+                <!-- the button to add products; this is for the mobile device only since in the desktop user can pick the product from the list in the right side of the form-->
+                <button type="button" class="btn btn-default hidden-lg">Add Product</button><hr>
+                <div class="row">
+                  
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-      <div class="box-body">
-        Start creating your amazing application!
+      <div class="col-lg-7 hidden-md hidden-sm hidden-xs">
+        <div class="box box-warning">
+          <!-- this is the initial default box colored as green meaning success but when things went bad it will turns yellow as warning -->
+        </div>
       </div>
-      <!-- /.box-body -->
-      <div class="box-footer">
-        Footer
-      </div>
-      <!-- /.box-footer-->
+
     </div>
-    <!-- /.box -->
+
 
   </section>
   <!-- /.content -->
+</div>
+
+<!-- Modal -->
+<div id="modalAddCustomer" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <form role="form" class="" action="" method="post">
+        <!-- modal header -->
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Add Customer</h4>
+        </div>
+        <!-- modal body -->
+        <div class="modal-body">
+          <div class="box-body">
+
+            <!-- form for add customer name-->
+            <div class="form-group">
+              <div class="input-group">
+                <!-- form and input groups are classes from bootstrap -->
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <input type="text" name="newCustomerName" placeholder="insert name" required
+                  id="newCustomerName" class="form-control input-lg">
+              </div>
+            </div>
+            <!-- form for insert customer document id -->
+            <div class="form-group">
+              <div class="input-group">
+                <!-- form and input groups are classes from bootstrap -->
+                <span class="input-group-addon"><i class="fa fa-file"></i></span>
+                <input type="number" name="newDocumentId" placeholder="Document ID" readonly
+                  id="newDocumentId" class="form-control input-lg" required>
+              </div>
+            </div>
+            <!-- form for customer email -->
+            <div class="form-group">
+              <div class="input-group">
+                <!-- form and input groups are classes from bootstrap -->
+                <span class="input-group-addon"><i class="fa fa-at"></i></span>
+                <input type="email" name="newCustomerEmail" placeholder="E mail" required
+                  class="form-control input-lg">
+              </div>
+            </div>
+            <!-- form for customer phone -->
+            <div class="form-group">
+              <div class="input-group">
+                <!-- form and input groups are classes from bootstrap -->
+                <span class="input-group-addon"><i class="fa fa-phone"></i></span>
+                <input type="text" name="newCustomerPhone" placeholder="nomor telepon" required
+                  class="form-control input-lg" data-inputmask="'mask': ['+(62) 999-9999-9999']"
+                  data-mask>
+              </div>
+            </div>
+            <!-- form for add customer address-->
+            <div class="form-group">
+              <div class="input-group">
+                <!-- form and input groups are classes from bootstrap -->
+                <span class="input-group-addon"><i class="fa fa-map-marker"></i></span>
+                <input type="text" name="newCustomerAddress" placeholder="Alamat" required
+                  class="form-control input-lg">
+              </div>
+            </div>
+            <!-- form for customer birth date -->
+            <div class="form-group">
+              <div class="input-group">
+                <!-- form and input groups are classes from bootstrap -->
+                <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                <input type="text" name="newCustomerBirthDate" placeholder="tanggal lahir" required
+                  class="form-control input-lg" data-inputmask="'alias': 'yyyy/mm/dd'"
+                  data-mask>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <!-- modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancel</button>
+          <button type="submit" class="btn btn-primary">Save Customer</button>
+        </div>
+        <!-- /.end model-content; -->
+        <?php
+          $addCustomer = new ControllerCustomer();
+          $addCustomer -> ctrAddCustomer();
+        ?>
+      </form>
+
+    </div>
+
+  </div>
 </div>
--- view/module/manage-sales.php
+++ view/module/manage-sales.php
@@ -53,7 +53,7 @@
               <td>2020-07-11 23:05:22</td>
               <td>
                 <div class="btn-group">
-                  <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                  <button class="btn btn-warning"><i class="fa fa-print"></i></button>
                   <button class="btn btn-danger"><i class="fa fa-times"></i></button>
                 </div>
               </td>
@@ -70,7 +70,7 @@
               <td>2020-07-11 23:05:22</td>
               <td>
                 <div class="btn-group">
-                  <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                  <button class="btn btn-warning"><i class="fa fa-print"></i></button>
                   <button class="btn btn-danger"><i class="fa fa-times"></i></button>
                 </div>
               </td>
@@ -89,74 +89,4 @@
 
   </section>
   <!-- /.content -->
-</div>
-<!-- this is the modal pop ups; -->
-<!-- Modal -->
-<div id="modalAddSupplier" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <form role="form" class="" action="" method="post">
-        <!-- modal header -->
-        <div class="modal-header" style="background:#3c8dbc; color:white">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Add Supplier</h4>
-        </div>
-        <!-- modal body -->
-        <div class="modal-body">
-          <div class="box-body">
-
-            <!-- form for add user name-->
-            <div class="form-group">
-              <div class="input-group">
-                <!-- form and input groups are classes from bootstrap -->
-                <span class="input-group-addon"><i class="fa fa-th"></i></span>
-                <input type="text" name="newSupplier" placeholder="insert name" required class="form-control input-lg">
-              </div>
-            </div>
-            <!-- input to select the role; -->
-            <div class="form-group">
-              <div class="input-group">
-                <!-- form and input groups are classes from bootstrap -->
-                <span class="input-group-addon"><i class="fa fa-users"></i></span>
-                <select class="form-control input-lg" name="newStatus">
-                  <option value="">Pilih Tipe Supplier</option>
-                  <option value="inhouse">Sendiri</option>
-                  <option value="consignment">Konsinyasi</option>
-                  <option value="rent">Sewa</option>
-                </select>
-              </div>
-            </div>
-            <!-- form for username -->
-            <div class="form-group">
-              <div class="input-group">
-                <!-- form and input groups are classes from bootstrap -->
-                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-                <input type="text" name="newBankAcc" placeholder="masukkan bank acc" required class="form-control input-lg">
-              </div>
-            </div>
-            <!-- form for password -->
-            <div class="form-group">
-              <div class="input-group">
-                <!-- form and input groups are classes from bootstrap -->
-                <span class="input-group-addon"><i class="fa fa-key"></i></span>
-                <input type="text" name="newAccNum" placeholder="masukkan nomor acc" required class="form-control input-lg">
-              </div>
-            </div>
-
-
-          </div>
-        </div>
-        <!-- modal footer -->
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancel</button>
-          <button type="submit" class="btn btn-primary">Save Supplier</button>
-        </div>
-        <!-- /.end model-content; -->
-      </form>
-
-    </div>
-
-  </div>
 </div>
